28ec49cdbf0d86e2bd0b1cb04bfbefe22fc45f20
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-05-06 18:00:45 +0300

    add dnd library components in kanban component

---
+++ src/components/card/index.jsx
@@ -0,0 +1,7 @@
+import './card.scss'
+
+const Card = (props) => {
+	return <div className='card'>{props.children}</div>
+}
+
+export default Card
--- src/components/kanban/index.jsx
+++ src/components/kanban/index.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import './kanban.scss'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+import mockData from '../../mockData.js'
 
 const Kanban = () => {
-	return <div>Kanban</div>
+	const [data, setData] = useState(mockData)
+
+	const onDragEnd = (result) => {
+		console.log(result)
+	}
+
+	return (
+		<div>
+			<DragDropContext onDragEnd={onDragEnd}>
+				<div className='kanban'>
+					{data.map((section) => (
+						<Droppable></Droppable>
+					))}
+				</div>
+			</DragDropContext>
+		</div>
+	)
 }
 
 export default Kanban
