7de6dec1024b8da039f70c1012364d9f4a1ae38f
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-05-06 18:13:50 +0300

    add styling to cards

--- src/components/kanban/index.jsx
+++ src/components/kanban/index.jsx
@@ -1,6 +1,8 @@
+import React, { useState } from 'react'
 import './kanban.scss'
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 import mockData from '../../mockData.js'
+import Card from '../card'
 
 const Kanban = () => {
 	const [data, setData] = useState(mockData)
@@ -14,7 +16,22 @@ const Kanban = () => {
 			<DragDropContext onDragEnd={onDragEnd}>
 				<div className='kanban'>
 					{data.map((section) => (
-						<Droppable></Droppable>
+						<Droppable key={section.id} droppableId={section.id}>
+							{(provided) => (
+								<div
+									{...provided.droppableProps}
+									className='kanban__section'
+									ref={provided.innerRef}
+								>
+									<div className='kanban__section__title'>{section.title}</div>
+									<div className='kanban__section__content'>
+										{section.tasks.map((task, index) => (
+											<Card>{task.title}</Card>
+										))}
+									</div>
+								</div>
+							)}
+						</Droppable>
 					))}
 				</div>
 			</DragDropContext>
