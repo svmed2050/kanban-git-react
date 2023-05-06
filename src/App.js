d3f2cfad7750647909745960e3b93609e9e833d9
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-05-06 20:10:37 +0300

    fetch data from github api open-assignee-closed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import Kanban from './components/kanban'
+import RepoInput from './components/repoinput'
 
 function App() {
 	return (
 		<div style={{ padding: '50px' }}>
 			<h1 style={{ marginBottom: '20px' }}>Kanban</h1>
+			<RepoInput />
 			<Kanban />
 		</div>
 	)
---
+++ src/components/repoinput/index.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+
+const RepoInput = () => {
+	const [repoUrl, setRepoUrl] = useState('')
+	const [issues, setIssues] = useState({ open: [], inProgress: [], done: [] })
+
+	const handleRepoLoad = async () => {
+		if (!repoUrl) return
+		const urlParts = repoUrl.split('/')
+		if (urlParts.length < 4) return
+
+		const newOwner = urlParts[3]
+		const newRepoName = urlParts[4]
+		const baseUrl = `https://api.github.com/repos/${newOwner}/${newRepoName}/issues?sort=created&direction=desc&per_page=4`
+
+		const urlOpen = `${baseUrl}&state=open`
+		const urlAssignee = `${baseUrl}&state=open&assignee=*`
+		const urlClosed = `${baseUrl}&state=closed`
+		const urls = [urlOpen, urlAssignee, urlClosed]
+
+		try {
+			const data = await Promise.all(
+				urls.map(async (url) => {
+					const resp = await fetch(url)
+					return resp.json()
+				})
+			)
+			setIssues({ open: data[0], inProgress: data[1], done: data[2] })
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	useEffect(() => {
+		console.log(issues)
+	}, [issues])
+
+	return (
+		<div>
+			<input
+				value={repoUrl}
+				onChange={(event) => setRepoUrl(event.target.value)}
+				type='text'
+			/>
+			<button onClick={handleRepoLoad}>Load</button>
+		</div>
+	)
+}
+
+export default RepoInput
