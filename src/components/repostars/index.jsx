578eaf2f5d04c7ae2e5e5da9bbd5be05d64d67fc
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-05-07 12:22:54 +0300

    add styling for star comopnent

--- src/components/repostars/index.jsx
+++ src/components/repostars/index.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import star from '../../icons/star-svgrepo-com.svg'
+import './repostars.scss'
 
 const RepoStars = ({ newOwner, newRepoName, gitStars }) => {
 	return (
-		<div>
+		<div className='stars-wrapper'>
 			<p>
 				{newOwner} &gt; {newRepoName}{' '}
 			</p>
 			<span>
-				<img src={star} alt='Star' width='20px' />
-				<span>
+				<img
+					className='stars-wrapper__img'
+					src={star}
+					alt='Star'
+					width='20px'
+				/>
+				<span className='stars-wrapper__quantity'>
 					{gitStars >= 1000 ? `${(gitStars / 1000).toFixed()}K` : gitStars}{' '}
 					stars
 				</span>
